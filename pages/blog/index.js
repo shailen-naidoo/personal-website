bdca206be24d8974589ad53ce85ba69c33207fb9
Shailen Naidoo <naidooshailen648@gmail.com>
2020-05-10 13:02:27 +0200

    Posts article'

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -11,16 +11,30 @@ const page = () => html`
   }
 
   time {
-    font-size: 12px;
+    font-size: 13px;
   }
 
   article * a {
     border-bottom: none !important;
     cursor: pointer;
   }
+
+  article {
+    margin-top: 30px;
+  }
 </style>
 <article>
   <h2><a>Web Workers, multi-threading in the browser</a></h2>
+  <p>Web Workers is a browser API which allows use to run JS off the main thread</p>
+  <time datetime="2020-10-07">07 October 2020</time>
+</article>
+<article>
+  <h2><a>Is postMessage slow?</a></h2>
+  <p>Web Workers is a browser API which allows use to run JS off the main thread</p>
+  <time datetime="2020-10-07">07 October 2020</time>
+</article>
+<article>
+  <h2><a>When should you be using Web Workers?</a></h2>
   <p>Web Workers is a browser API which allows use to run JS off the main thread</p>
   <time datetime="2020-10-07">07 October 2020</time>
 </article>
