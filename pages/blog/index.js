4049bc49730f14df6ec103ca3855870489b3704c
Shailen Naidoo <naidooshailen648@gmail.com>
2020-05-10 12:38:14 +0200

    Setting up the blog index

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,31 @@
+const html = require('html-template-tag')
+
+const page = () => html`
+<style>
+  h2 {
+    margin-bottom: 5px;
+  }
+
+  p {
+    margin-bottom: 5px;
+  }
+
+  time {
+    font-size: 12px;
+  }
+
+  article * a {
+    border-bottom: none !important;
+    cursor: pointer;
+  }
+</style>
+<article>
+  <h2><a>Web Workers, multi-threading in the browser</a></h2>
+  <p>Web Workers is a browser API which allows use to run JS off the main thread</p>
+  <time datetime="2020-10-07">07 October 2020</time>
+</article>
+`
+
 module.exports = {
-  page: () => ''
+  page,
 }
