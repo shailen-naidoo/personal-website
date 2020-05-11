b963732f9e3accc85e60d2d4dc346ea785c98919
Shailen Naidoo <naidooshailen648@gmail.com>
2020-05-11 11:16:29 +0200

    Minor change

--- hydrogen.hooks.js
+++ hydrogen.hooks.js
@@ -17,11 +17,11 @@ const beforeServiceWorkerGenerated = async (ctx) => {
   `)
 
   const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${urls.join('\n')}  
-    </urlset>
-  `
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.join('\n')}  
+</urlset>
+  `.replace('\n', '')
 
   await fs.outputFile(`${cwd}/sitemap.xml`, sitemap)
 }
