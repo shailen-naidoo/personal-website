b035529f5dd8455393058615b4f97d3baa54b9c5
Shailen Naidoo <naidooshailen648@gmail.com>
2020-05-11 11:13:32 +0200

    Added a sitemap

--- hydrogen.hooks.js
+++ hydrogen.hooks.js
@@ -1,4 +1,7 @@
 const { minify } = require('html-minifier')
+const fs = require('fs-extra')
+
+const cwd = process.cwd()
 
 const beforeEachPageSaved = (ctx) => {
   const { html } = ctx
@@ -6,6 +9,24 @@ const beforeEachPageSaved = (ctx) => {
   ctx.html = minify(html, { collapseWhitespace: true, minifyCSS: true, minifyJS: true })
 }
 
+const beforeServiceWorkerGenerated = async (ctx) => {
+  const urls = ctx.routes.map(({ route }) => `
+    <url>
+      <loc>https://shailen.dev${route}</loc>
+    </url>
+  `)
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls.join('\n')}  
+    </urlset>
+  `
+
+  await fs.outputFile(`${cwd}/sitemap.xml`, sitemap)
+}
+
 module.exports = {
   beforeEachPageSaved,
+  beforeServiceWorkerGenerated
 }
