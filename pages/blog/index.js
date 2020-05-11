56e2a00171e25f488024b36512733ceab6022665
Shailen Naidoo <naidooshailen648@gmail.com>
2020-05-11 09:22:12 +0200

    Created summary cards

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,27 @@
+const fs = require('fs-extra')
 const html = require('html-template-tag')
 
-const page = () => html`
+const cwd = process.cwd()
+
+const generateArticleCards = async () => {
+  const postFilePaths = await fs.readdir(`${cwd}/public/posts`)
+  const posts = await Promise.all(postFilePaths.map(async (path) => {
+    const data = await fs.readJSON(`${cwd}/public/posts/${path}`)
+    const [slug] = path.split('.')
+
+    return html`
+      <article>
+        <h2><a href="/blog/posts/${slug}">${data.title}</a></h2>
+        <p>${data.summary}</p>
+        <time datetime="${data.created_at}">${data.created_at}</time>
+      </article>
+    `
+  }))
+
+  return posts
+}
+
+const page = async () => html`
 <style>
   h2 {
     margin-bottom: 5px;
@@ -20,10 +41,11 @@ const page = () => html`
   }
 
   article {
-    margin-top: 30px;
+    /* margin-top: 30px; */
   }
 </style>
-<article>
+${await generateArticleCards()}
+<!-- <article>
   <h2><a>Web Workers, multi-threading in the browser</a></h2>
   <p>Web Workers is a browser API which allows use to run JS off the main thread</p>
   <time datetime="2020-10-07">07 October 2020</time>
@@ -37,7 +59,7 @@ const page = () => html`
   <h2><a>When should you be using Web Workers?</a></h2>
   <p>Web Workers is a browser API which allows use to run JS off the main thread</p>
   <time datetime="2020-10-07">07 October 2020</time>
-</article>
+</article> -->
 `
 
 module.exports = {
